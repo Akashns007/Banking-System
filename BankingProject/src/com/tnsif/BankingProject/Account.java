34195e7d322ccede68ba6c14e5300b1a1b7d4c49
Akashns007 <mrpeculiar007@gmail.com>
2024-09-06 21:12:44 +0530

    first commit

---
+++ BankingProject/src/com/tnsif/BankingProject/Account.java
@@ -0,0 +1,60 @@
+package com.tnsif.BankingProject;
+
+public class Account {
+    private int accountID;
+    private int customerID;
+    private String type;
+    private double balance;
+
+    // Constructor
+    public Account(int accountID, int customerID, String type, double balance) {
+        this.accountID = accountID;
+        this.customerID = customerID;
+        this.type = type;
+        this.balance = balance;
+    }
+
+    // Getters and Setters
+    public int getAccountID() {
+        return accountID;
+    }
+
+    public void setAccountID(int accountID) {
+        this.accountID = accountID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    // Overridden toString method
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountID=" + accountID +
+                ", customerID=" + customerID +
+                ", type='" + type + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
